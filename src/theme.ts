d4c26c3d95ea918bcea98d349476c803f6c9a9c9
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-12-09 01:27:00 +0900

    기본설정

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textClolr: "black",
+  accentColor: "blue",
+  cardBgColor: "gray",
+};
